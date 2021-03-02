2651856a7689d1d097838a0d4e95dfd69e4df7f2
Fanni Csincsák <fanni@toptal.com>
2021-03-02 17:13:11 +0100

    2nd table revised

--- scripts/tables.js
+++ scripts/tables.js
@@ -17,3 +17,23 @@ $(document).ready(function() {
         ]
     } );
 } );
+
+//Scroll Table
+$(document).ready(function() {
+    $('#example2').DataTable( {
+        "scrollY": 300,
+        "scrollX": true,
+        "paging": false,
+        "searching": false,
+        "info": false,
+        "ajax": "data/myData",
+        "columns": [
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "extn" },
+            { "data": "start_date" },
+            { "data": "salary" }
+        ]
+    } );
+} );
