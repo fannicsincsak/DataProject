a1aa23396e7f8d44319741eeb8585f4ecef52888
Fanni Csincsák <fanni@toptal.com>
2021-03-04 20:09:49 +0100

    Chart CSS and data changes

--- scripts/charts.js
+++ scripts/charts.js
@@ -2,36 +2,55 @@ var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+            label: 'PREDICTED_CONIFER_VOLUME',
+            data: [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025],
+            backgroundColor: "#00A5BB",
+        },
+            {
+            label: 'PREDICTED_DECID_VOLUME',
+            data: [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661],
+            backgroundColor: "#D5F0F3"
+            }
+        ]
     },
     options: {
         scales: {
             yAxes: [{
                 ticks: {
                     beginAtZero: true
+                },
+                gridLines: {
+                    borderDash: [4, 5],
+                    color: "#EAEDF2"
                 }
-            }]
+            }],
+            xAxes : [ {
+                gridLines : {
+                    display : false
+                },
+                stacked: true,
+                barPercentage: 0.4,
+            } ]
+        },
+        layout: {
+            padding: {
+                left: 16,
+                right:16,
+                top: 16,
+                bottom: 16
+            }
+        },
+        legend: {
+            labels: {
+                defaultFontColor: '#555A64',
+                defaultFontSize: 12,
+                defaultFontFamily: "'Roboto', sans-serif;",
+                boxWidth: 12,
+            },
+            position: 'bottom',
+            align: 'center',
         }
-    }
+    },
 });
