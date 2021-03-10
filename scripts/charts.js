be2b61e055881754796d1cef190d28e551603f08
Fanni Csincsák <fanni@toptal.com>
2021-03-10 20:56:57 +0100

    Chart Legend Plugin Fixes

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,6 +1,6 @@
-var ChartistPluginScript = document.createElement('script');
-ChartistPluginScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/chartist-plugin-legend/0.6.2/chartist-plugin-legend.min.js');
-document.head.appendChild(ChartistPluginScript);
+//var ChartistPluginScript = document.createElement('script');
+//ChartistPluginScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/chartist-plugin-legend/0.6.2/chartist-plugin-legend.js');
+//document.head.appendChild(ChartistPluginScript);
 
 //General for all Charts
 var options = {
@@ -10,7 +10,6 @@ var options = {
     height: 450,
     plugins: [
         Chartist.plugins.legend({
-            legendNames: ['Custom title', 'Another one'],
         })
     ]
 };
@@ -31,8 +30,8 @@ var responsiveOptions = [
 var data1 = {
     labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
     series: [
-        [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025],
-        [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]
+        { "name": "A", "data": [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
+        { "name": "B", "data": [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]}
     ]
 };
 
@@ -40,8 +39,8 @@ var data1 = {
 var data2 = {
     labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
     series: [
-        [12685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025],
-        [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]
+        { "name": "A", "data": [12685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
+        { "name": "B", "data": [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]},
     ]
 };
 
