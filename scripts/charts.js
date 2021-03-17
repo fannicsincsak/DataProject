1304eac21d528a84efd2133864f838d43857d1d0
Fanni Csincsák <fanni@toptal.com>
2021-03-17 21:06:04 +0100

    Lot of fixes

--- scripts/charts.js
+++ scripts/charts.js
@@ -14,7 +14,8 @@ var options = {
     ]
 };
 
-var optionsarea = {
+// Options for Area Chart only
+var optionsAreaChart = {
     seriesBarDistance: 10,
     chartPadding: 32,
     scaleMinSpace: 100,
@@ -26,6 +27,44 @@ var optionsarea = {
         })
     ]
 };
+
+// Options for Stacked Bar Chart only
+var optionsStackedBarChart = {
+    seriesBarDistance: 10,
+    chartPadding: 32,
+    scaleMinSpace: 100,
+    height: 450,
+    low: 0,
+    showArea: true,
+    stackBars: true,
+    axisY: {
+        labelInterpolationFnc: function(value) {
+            return (value / 1000) + 'k';
+        }
+    },
+    plugins: [
+        Chartist.plugins.legend({
+        })
+    ]
+};
+
+// Options for Horizontal Bar Chart only
+var optionsHorizontalBarChart = {
+    seriesBarDistance: 6,
+    chartPadding: 32,
+    scaleMinSpace: 100,
+    height: 450,
+    reverseData: true,
+    horizontalBars: true,
+    axisY: {
+        offset: 70
+    },
+    plugins: [
+        Chartist.plugins.legend({
+        })
+    ]
+};
+
 
 var responsiveOptions = [
     ['screen and (max-width: 640px)', {
@@ -40,7 +79,25 @@ var responsiveOptions = [
 
 
 // Simple Bar Chart Data
-var data1 = {
+var dataBarChart = {
+    labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
+    series: [
+        { "name": "Category A", "data": [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
+        { "name": "Category B", "data": [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]}
+    ]
+};
+
+// Stacked Bar Chart Data
+var dataStackedBarChart = {
+    labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
+    series: [
+        { "name": "Category A", "data": [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
+        { "name": "Category B", "data": [51741,38662,56018,67774,48112,115091,6245,122229,71327,96959,19764,10036,47343,13470,33991,26924,63854,47661]}
+    ]
+};
+
+// Horizontal Bar Chart Data
+var dataHorizontalBarChart = {
     labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
     series: [
         { "name": "Category A", "data": [6685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
@@ -49,7 +106,7 @@ var data1 = {
 };
 
 // Simple Line Chart Data
-var data2 = {
+var dataLineChart = {
     labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
     series: [
         { "name": "Category A", "data": [12685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
@@ -58,7 +115,7 @@ var data2 = {
 };
 
 // Area Line Chart Data
-var data3 = {
+var dataAreaLineChart = {
     labels: ['070124a','71104','71224','72254','73124','73194','76184','78024','78154','82015','84035','84074','85045','86124','86234','87244','91124','91234'],
     series: [
         { "name": "Category A", "data": [12685,10784,15066,9861,8074,22953,2294,28571,24586,12475,3502,1079,8668,2748,20086,12966,15997,15025]},
@@ -67,13 +124,19 @@ var data3 = {
 };
 
 // Create Bar Chart
-new Chartist.Bar('#chart1', data1, options, responsiveOptions);
+new Chartist.Bar('#chart1', dataBarChart, options, responsiveOptions);
+
+// Create Stacked Bar Chart
+new Chartist.Bar('#chart2', dataStackedBarChart, optionsStackedBarChart, responsiveOptions);
+
+// Create Horizontal Bar Chart
+new Chartist.Bar('#chart3', dataHorizontalBarChart, optionsHorizontalBarChart, responsiveOptions);
 
 // Create Line Chart
-new Chartist.Line('#chart2', data2, options, responsiveOptions);
+new Chartist.Line('#chart4', dataLineChart, options, responsiveOptions);
 
-// Create Line Chart
-new Chartist.Line('#chart3', data3,  optionsarea, responsiveOptions);
+// Create Area Line Chart
+new Chartist.Line('#chart5', dataAreaLineChart, optionsAreaChart, responsiveOptions);
 
 
 
